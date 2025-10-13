b1a03418a3bf84285c5657543c11324131d96e35
Dinh Xuan Hien <124948520+hiendepzainn@users.noreply.github.com>
2025-10-13 16:10:37 +0700

    done: hello world

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("Hello world!");
+const express = require("express");
+const app = express();
+
+const PORT = 1410;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/abc", (req, res) => {
+  res.send("Hello Dinh Xuan Hien!");
+});
+
+app.listen(PORT, () => {
+  console.log(`This app is running at port: ${PORT}`);
+});
