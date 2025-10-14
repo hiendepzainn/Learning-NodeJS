d8fb57a21c3087b2df71441c6024151d1b51afe8
Dinh Xuan Hien <124948520+hiendepzainn@users.noreply.github.com>
2025-10-14 09:33:26 +0700

    update source

--- dist/app.js
+++ dist/app.js
@@ -1,16 +1,13 @@
 const express = require("express");
 const app = express();
-
 const PORT = 1410;
-
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+    res.send("Hello World update!");
 });
-
 app.get("/abc", (req, res) => {
-  res.send("Hello Dinh Xuan Hien!");
+    res.send("Hello Dinh Xuan Hien!");
 });
-
 app.listen(PORT, () => {
-  console.log(`This app is running at port: ${PORT}`);
+    console.log(`This app is running at port: ${PORT}`);
 });
+//# sourceMappingURL=app.js.map
