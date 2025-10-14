49ca88a8dcb12328eb078fbb289bd9e622b1e38c
Dinh Xuan Hien <124948520+hiendepzainn@users.noreply.github.com>
2025-10-14 08:04:22 +0700

    setting TypeScript compiler

---
+++ dist/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const PORT = 1410;
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+app.get("/abc", (req, res) => {
+    res.send("Hello Dinh Xuan Hien!");
+});
+app.listen(PORT, () => {
+    console.log(`This app is running at port: ${PORT}`);
+});
+//# sourceMappingURL=index.js.map
