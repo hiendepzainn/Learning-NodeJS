7977620282d3f8cd1ef95f0c3ca9032d85416121
Dinh Xuan Hien <124948520+hiendepzainn@users.noreply.github.com>
2025-10-22 15:59:54 +0700

    done: use Dashboard template

--- src/routes/web.ts
+++ src/routes/web.ts
@@ -7,17 +7,16 @@ import {
   postDeleteUser,
   postUpdateUser,
 } from "../controllers/user.controller";
+import { getDashboardPage } from "../controllers/admin/dashboard.controller";
 
 export const initRouters = (app: Express) => {
   app.get("/", getHomePage);
-
   app.get("/create-user", getCreateUserPage);
-
   app.post("/handle-create-user", postCreateUser);
-
   app.post("/handle-delete-user/:id", postDeleteUser);
-
   app.get("/handle-view-user/:id", getViewUser);
-
   app.post("/handle-update-user/:id", postUpdateUser);
+
+  // ADMIN
+  app.get("/admin", getDashboardPage);
 };
