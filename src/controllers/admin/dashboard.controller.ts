d115b095d523d990781c1bbda12df46fa4049643
Dinh Xuan Hien <124948520+hiendepzainn@users.noreply.github.com>
2025-10-23 10:41:32 +0700

    done: take UI user management to Layout

--- src/controllers/admin/dashboard.controller.ts
+++ src/controllers/admin/dashboard.controller.ts
@@ -1,11 +1,23 @@
 import { Request, Response } from "express";
+import { getAllUsers } from "../../services/user.service";
 
 const getDashboardPage = (req: Request, res: Response) => {
   return res.render("admin/dashboard/show.ejs");
 };
 
-const getUserPage = (req: Request, res: Response) => {
-  return res.render("admin/user/show.ejs");
+const getUserPage = async (req: Request, res: Response) => {
+  const users = await getAllUsers();
+  return res.render("admin/user/show.ejs", {
+    listUsers: users,
+  });
 };
 
-export { getDashboardPage, getUserPage };
+const getOrderPage = (req: Request, res: Response) => {
+  return res.render("admin/order/show.ejs");
+};
+
+const getProductPage = (req: Request, res: Response) => {
+  return res.render("admin/product/show.ejs");
+};
+
+export { getDashboardPage, getUserPage, getOrderPage, getProductPage };
--- src/routes/web.ts
+++ src/routes/web.ts
@@ -9,12 +9,14 @@ import {
 } from "../controllers/user.controller";
 import {
   getDashboardPage,
+  getOrderPage,
+  getProductPage,
   getUserPage,
 } from "../controllers/admin/dashboard.controller";
 
 export const initRouters = (app: Express) => {
   app.get("/", getHomePage);
-  app.get("/create-user", getCreateUserPage);
+
   app.post("/handle-create-user", postCreateUser);
   app.post("/handle-delete-user/:id", postDeleteUser);
   app.get("/handle-view-user/:id", getViewUser);
@@ -23,4 +25,7 @@ export const initRouters = (app: Express) => {
   // ADMIN
   app.get("/admin", getDashboardPage);
   app.get("/admin/user", getUserPage);
+  app.get("/admin/create-user", getCreateUserPage);
+  app.get("/admin/order", getOrderPage);
+  app.get("/admin/product", getProductPage);
 };
