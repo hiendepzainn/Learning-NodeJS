476f9d37bc4e1dab6cfa554bbcdf2c5706ecdca8
Dinh Xuan Hien <124948520+hiendepzainn@users.noreply.github.com>
2025-11-18 17:49:34 +0700

    update getCartPage (/cart)

--- src/controllers/client/cart.controller.ts
+++ src/controllers/client/cart.controller.ts
@@ -1,6 +1,10 @@
 import { Request, Response } from "express";
 
 const getCartPage = async (req: Request, res: Response) => {
+  const user = req.user;
+  if (!user) {
+    return res.redirect("/login");
+  }
   res.render("client/cart/cart.ejs");
 };
 
