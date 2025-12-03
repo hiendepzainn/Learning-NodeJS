23883d10eb1733e18f03c5d29ccda20915891110
Dinh Xuan Hien <124948520+hiendepzainn@users.noreply.github.com>
2025-12-03 10:55:53 +0700

    split file: Web.ts --> clientRouter.ts

---
+++ src/routes/clientRouter.ts
@@ -0,0 +1,68 @@
+import { Router } from "express";
+import { getHomePage } from "../controllers/user.controller";
+import {
+  getProductPageClient,
+  getProductsPage,
+  postAddProductToCart,
+  postAddProductWithQuantity,
+  postAddToCartFilter,
+} from "../controllers/client/product.controller";
+import {
+  getCartPage,
+  getCheckOutPage,
+  getThankyouPage,
+  postConfirmCart,
+  postCreateOrder,
+  postDeleteCartDetailByID,
+} from "../controllers/client/cart.controller";
+import { getOrderHistoryPage } from "../controllers/admin/order.controller";
+import { checkLogin } from "../middleware/auth";
+import {
+  getLoginPage,
+  getRegisterPage,
+  getSuccessLoginPage,
+  postLogout,
+  postRegister,
+} from "../controllers/authentication.controller";
+import passport from "passport";
+
+const clientRouter = Router();
+
+// CLIENT
+clientRouter.get("/", getHomePage);
+clientRouter.get("/product/:id", getProductPageClient);
+clientRouter.get("/products", getProductsPage);
+
+// Product (client)
+clientRouter.post("/addProductToCart/:id", postAddProductToCart);
+clientRouter.post(
+  "/add-product-to-cart-with-quantity",
+  postAddProductWithQuantity
+);
+clientRouter.post("/addToCartFilter/:id", postAddToCartFilter);
+
+// Cart (client)
+clientRouter.get("/cart", getCartPage);
+clientRouter.post("/deleteCartDetail/:id", postDeleteCartDetailByID);
+clientRouter.get("/checkout", getCheckOutPage);
+clientRouter.post("/confirm-cart", postConfirmCart);
+clientRouter.post("/create-order", postCreateOrder);
+clientRouter.get("/thankyou", getThankyouPage);
+clientRouter.get("/order-history", getOrderHistoryPage);
+
+// Authentication
+clientRouter.get("/login", checkLogin, getLoginPage);
+clientRouter.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/successLoginPage",
+    failureRedirect: "/login",
+    failureMessage: true,
+  })
+);
+clientRouter.get("/register", checkLogin, getRegisterPage);
+clientRouter.post("/register", postRegister);
+clientRouter.get("/successLoginPage", getSuccessLoginPage);
+clientRouter.post("/logout", postLogout);
+
+export default clientRouter;
--- src/routes/web.ts
+++ src/routes/web.ts
@@ -1,68 +1,15 @@
 import { Express } from "express";
-import { getHomePage } from "../controllers/user.controller";
-import {
-  getProductPageClient,
-  getProductsPage,
-  postAddProductToCart,
-  postAddProductWithQuantity,
-  postAddToCartFilter,
-} from "../controllers/client/product.controller";
-import {
-  getLoginPage,
-  getRegisterPage,
-  getSuccessLoginPage,
-  postLogout,
-  postRegister,
-} from "../controllers/authentication.controller";
-import passport from "passport";
-import { checkAdmin, checkLogin } from "../middleware/auth";
-import { get404Page } from "../controllers/status/status.controller";
+
 import adminRouter from "./adminRouter";
-import {
-  getCartPage,
-  getCheckOutPage,
-  postDeleteCartDetailByID,
-  postConfirmCart,
-  postCreateOrder,
-  getThankyouPage,
-} from "../controllers/client/cart.controller";
-import { getOrderHistoryPage } from "../controllers/admin/order.controller";
 import APIRouter from "./api";
+import clientRouter from "./clientRouter";
+
+import { checkAdmin } from "../middleware/auth";
+import { get404Page } from "../controllers/status/status.controller";
 
 export const initRouters = (app: Express) => {
   // CLIENT
-  app.get("/", getHomePage);
-  app.get("/product/:id", getProductPageClient);
-  app.get("/products", getProductsPage);
-
-  // Product (client)
-  app.post("/addProductToCart/:id", postAddProductToCart);
-  app.post("/add-product-to-cart-with-quantity", postAddProductWithQuantity);
-  app.post("/addToCartFilter/:id", postAddToCartFilter);
-
-  // Cart (client)
-  app.get("/cart", getCartPage);
-  app.post("/deleteCartDetail/:id", postDeleteCartDetailByID);
-  app.get("/checkout", getCheckOutPage);
-  app.post("/confirm-cart", postConfirmCart);
-  app.post("/create-order", postCreateOrder);
-  app.get("/thankyou", getThankyouPage);
-  app.get("/order-history", getOrderHistoryPage);
-
-  // Authentication
-  app.get("/login", checkLogin, getLoginPage);
-  app.post(
-    "/login",
-    passport.authenticate("local", {
-      successRedirect: "/successLoginPage",
-      failureRedirect: "/login",
-      failureMessage: true,
-    })
-  );
-  app.get("/register", checkLogin, getRegisterPage);
-  app.post("/register", postRegister);
-  app.get("/successLoginPage", getSuccessLoginPage);
-  app.post("/logout", postLogout);
+  app.use("", clientRouter);
 
   // ADMIN - check access ADMIN page (handle 403 Forbidden page)
   app.use("/admin", checkAdmin, adminRouter);
