ebd911d45f10a0b57baf069860f99744c0c53776
Dinh Xuan Hien <124948520+hiendepzainn@users.noreply.github.com>
2026-05-14 16:18:30 +0700

    fix: vnpay constant move to .ENV

--- src/services/payment.service.ts
+++ src/services/payment.service.ts
@@ -1,11 +1,14 @@
 import { ProductCode, VnpLocale, dateFormat } from "vnpay";
 import { VNPay } from "vnpay";
 import { prisma } from "../config/client";
+import "dotenv/config";
 
 const createURL = (orderID) => {
+  const secureSecret = process.env.SECURE_SECRET || "xxxxxx";
+  const tmnCode = process.env.TMN_CODE || "yyyyyy";
   const vnpay = new VNPay({
-    secureSecret: "E4H8DFZID84JSB1IZ9K5W074HQMOHDJK",
-    tmnCode: "5YOHHQJO",
+    secureSecret: secureSecret,
+    tmnCode: tmnCode,
     testMode: true,
   });
 
@@ -13,7 +16,7 @@ const createURL = (orderID) => {
   tomorrow.setDate(tomorrow.getDate() + 1);
 
   const paymentUrl = vnpay.buildPaymentUrl({
-    vnp_Amount: 2000000,
+    vnp_Amount: 10000,
     vnp_IpAddr: "127.0.0.1",
     vnp_TxnRef: orderID,
     vnp_OrderInfo: "Thanh toan don hang 123456",
